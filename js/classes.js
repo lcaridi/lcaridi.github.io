3757c512adac9d86fbe2e9af6d9a1dc853efe5bb
lucio caridi <lcaridi@technisys.com>
2020-02-29 22:30:12 -0300

    initial commit

---
+++ js/classes.js
@@ -0,0 +1,15 @@
+// class Gif {
+//     constructor(url, title, hashtags) {
+//       this.url = url;
+//       this.title = title;
+//       this.hashtags = hashtags;
+//     }
+//   }
+//   class Gif {
+//     constructor(url, title, hashtags) {
+//       this.url = url;
+//       this.title = title;
+//       this.hashtags = hashtags;
+//     }
+//   }
+  
---
+++ js/index.js
@@ -0,0 +1,67 @@
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const searchSuggested = document.getElementById('searchSuggested');
+const lastSearchs = document.getElementById('searchlastSearchs');
+const searchButtons = document.getElementsByClassName('searchButtons');
+const buttonIcon = document.getElementById('buttonIcon');
+const buttonSeeMore = document.getElementsByClassName('buttonSeeMore');
+const firstDiv = document.getElementById('firstDiv');
+const secondDiv = document.getElementById('secondDiv');
+const thirdDiv = document.getElementById('thirdDiv');
+const fourthDiv = document.getElementById('fourthDiv');
+
+getTendenciesGifs(gifsResultsGrid);
+
+const searchEventListener = () => {
+  searchGifs(searchInput.value, gifsResultsGrid);
+  gifsResultsText.innerText = searchInput.value;
+  gifsResultsText.scrollIntoView();
+  saveSearchs(searchInput.value.trim());
+  searchInput.value = '';
+  searchSuggested.style.display = 'none';
+  lastSearchs.style.display = 'block';
+  lastSearchs.style.display = 'flex';
+};
+
+searchButton.addEventListener('click', searchEventListener);
+
+searchInput.addEventListener('keyup', (e) => {
+  if (searchInput.value.trim() !== '' && e.keyCode === 13) {
+    searchEventListener();
+  }
+});
+
+Array.from(searchButtons).forEach(element => {
+  element.addEventListener('click', () => {
+    gifsResultsText.innerText = element.textContent;
+    searchSuggested.style.display = 'none';
+    searchGifs(element.childNodes[0].nodeValue, gifsResultsGrid);
+    gifsResults.scrollIntoView();
+  });
+});
+
+searchInput.addEventListener('keyup', () => {
+  if (searchInput.value.trim() !== '') {
+    searchButton.removeAttribute('disabled');
+    searchButton.classList.add('button--primary');
+    searchSuggested.style.display = 'block';
+    buttonIcon.style.opacity = 1;
+  } else {
+    searchButton.setAttribute('disabled', '');
+    searchButton.classList.remove('button--primary');
+    buttonIcon.style.opacity = 0.3;
+    searchSuggested.style.display = 'none';
+  }
+});
+
+Array.from(buttonSeeMore).forEach(element => {
+  element.addEventListener('click', () => {
+    searchGifs(element.name, gifsResultsGrid);
+    gifsResults.scrollIntoView();
+  });
+});
+
+searchCategoryGifs('thanos', firstDiv);
+searchCategoryGifs('linux', secondDiv);
+searchCategoryGifs('baby yoda', thirdDiv);
+searchCategoryGifs('sailor moon', fourthDiv);
